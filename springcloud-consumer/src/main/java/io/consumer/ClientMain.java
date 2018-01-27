5843daa20e214a1200e9802d4fd0490d6b9ef2e3
yaohaishi <yaohaishi@huawei.com>
2018-01-27 22:40:53 +0800

    接入服务中心和配置中心，注册到服务中心

--- springcloud-consumer/src/main/java/io/consumer/ClientMain.java
+++ springcloud-consumer/src/main/java/io/consumer/ClientMain.java
@@ -1,13 +1,16 @@
 package io.consumer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableFeignClients
+@EnableAutoConfiguration
+@ImportResource(locations = "classpath*:META-INF/spring/*.bean.xml")
 public class ClientMain {
 
   public static void main(String[] args) {
--- springcloud-provider/src/main/java/io/provider/ServerMain.java
+++ springcloud-provider/src/main/java/io/provider/ServerMain.java
@@ -1,14 +1,17 @@
 package io.provider;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+@EnableAutoConfiguration
+@ImportResource(locations = "classpath*:META-INF/spring/*.bean.xml")
 public class ServerMain {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerMain.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ServerMain.class, args);
+  }
 }
